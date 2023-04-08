22000b4e6aec14fbea63a3e2a1156a995c2fa179
watergod360 <watergod360@gmail.com>
2023-04-08 14:43:53 -0400

    Basic Chatbot

--- pages/index.tsx
+++ pages/index.tsx
@@ -165,7 +165,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat With Your Legal Docs
+            Chat With a Peacebuilding Evaluation Bot
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
@@ -282,7 +282,7 @@ export default function Home() {
                     placeholder={
                       loading
                         ? 'Waiting for response...'
-                        : 'What is this legal case about?'
+                        : 'What questions about peacebuilding theory do you have?'
                     }
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
@@ -320,7 +320,7 @@ export default function Home() {
         </div>
         <footer className="m-auto p-4">
           <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
+            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin) and adapted by Logan Malone.
           </a>
         </footer>
       </Layout>
